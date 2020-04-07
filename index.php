108a1a127c363ff6e03ec37859004c2b091d2bef
jorge <jorge@DESKTOP-3QMH1VG>
2020-04-07 17:46:52 -0500

    login

--- index.php
+++ index.php
@@ -1,26 +1,38 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "hola joven";
-        echo "holaaaaaaaaaa";
-        
-        echo "prueba app escritorio ";
-         echo "prueba app escritorio ";
-          echo "prueba app escritorio ";
-           echo "prueba app escritorio ";
-          
-         
-         
-        ?>
-    </body>
-</html>
+<?php
+include_once 'includes/user.php';
+include_once 'includes/user_session.php';
+
+
+$userSession = new UserSession();
+$user = new User();
+
+if(isset($_SESSION['user'])){
+    //echo "hay sesion";
+    $user->setUser($userSession->getCurrentUser());
+    include_once 'vistas/home.php';
+
+}else if(isset($_POST['username']) && isset($_POST['password'])){
+    
+    $userForm = $_POST['username'];
+    $passForm = $_POST['password'];
+
+    $user = new User();
+    if($user->userExists($userForm, $passForm)){
+        //echo "Existe el usuario";
+        $userSession->setCurrentUser($userForm);
+        $user->setUser($userForm);
+
+        include_once 'vistas/home.php';
+    }else{
+        //echo "No existe el usuario";
+        $errorLogin = "Nombre de usuario y/o password incorrecto";
+        include_once 'vistas/login.php';
+    }
+}else{
+    //echo "login";
+    include_once 'vistas/login.php';
+}
+
+
+
+?>
