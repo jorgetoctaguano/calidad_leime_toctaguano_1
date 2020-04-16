3954721f32b7e73dfc3a2f9ed11e7cc01553e154
Andrews <Andrews@Andres_Leime>
2020-04-15 21:11:01 -0500

    modifique el formulario a la vez que la conexion css y validacion basica de los campos como si estan vacios o que sean solo numeros en cedula y celular

--- vistas/formulario.php
+++ vistas/formulario.php
@@ -1,61 +1,46 @@
+<?php
+         if(isset ($_POST['submit']))
+             { 
+                 $cedula = $_POST['cedula'];            
+                 $nombre = $_POST['nombre'];
+                 $apellidos = $_POST['apellidos'];
+                 $celular = $_POST['celular'];
+		 $email = $_POST['email'];
+                 $ciudad = $_POST['ciudad'];
+		 $direccion = $_POST['direccion'];
+                 $fecha = $_POST['fechaN'];
+                 $edad = $_POST['edad'];
+		 $estado = $_POST['estadoC'];
+             
+             }
+				
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+        <title>Validar formulario</title>
 	<meta charset="UTF-8">
-	<title>Validar formulario</title>
-	<link rel="stylesheet" href="css/formulario.css">
+        <link rel="stylesheet" type="text/css" href="../css/formulario.css">
 </head>
 <body>
-	<form action="formulario.php" method="POST">
-		<?php
-			
-			if(isset($_POST['nombre'])){
-				$nombre = $_POST['nombre'];
-				$password = $_POST['password'];
-				$email = $_POST['email'];
-
-				$campos = array();
-
-				if($nombre == ""){
-					array_push($campos, "El campo Nombre no pude estar vacío");
-				}
-
-				if($password == "" || strlen($password) < 6){
-					array_push($campos, "El campo Password no puede estar vacío, ni tener menos de 6 caracteres.");
-				}
-
-				if($email == "" || strpos($email, "@") === false){
-					array_push($campos, "Ingresa un correo electrónico válido.");
-				}
-
-				if(count($campos) > 0){
-					echo "<div class='error'>";
-					for($i = 0; $i < count($campos); $i++){
-						echo "<li>".$campos[$i]."</i>";
-					}
-				}else{
-					echo "<div class='correcto'>
-							Datos correctos";
-				}
-				echo "</div>";
-			}
-		?>
-		<p>
-		Nombre:<br/>
-		<input type="text" name="nombre">
-		</p>
-
-		<p>
-		Password:<br/>
-		<input type="password" name="password">
-		</p>
-
-		<p>
-		correo electrónico:<br/>
-		<input type="text" name="email">
-		</p>
-
-		<p><input type="submit" value="enviar datos"></p> 
+    <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF']);?>" method="POST">
+                <p>Cedula:<br/><input type="text" name="cedula" value="<?php if(isset($cedula))echo $cedula?>"></p>
+		<p> Nombres:<br/> <input type="text" name="nombre" value="<?php if(isset($nombre))echo $nombre?>"></p>
+                <p> Apellidos:<br/> <input type="text" name="apellidos" value="<?php if(isset($apellidos))echo $apellidos?>"> </p>
+                <p>Ciudad:<br/><input type="text" name="ciudad" value="<?php if(isset($ciudad))echo $ciudad?>">></p>
+		<p> Tel Celular:<br/> <input type="text" name="celular" value="<?php if(isset($celular))echo $celular?>"></p>
+                <p> Dirección:<br/> <input type="text" name="direccion" value="<?php if(isset($direccion))echo $direccion?>"> </p>
+                <p>correo electrónico:<br/><input type="text" name="email" value="<?php if(isset($email))echo $email?>"></p>
+                <p> Fecha Nacimiento:<br/> <input type="text" name="fechaN"></p>
+                <p> Edad:<br/> <input type="text" name="edad"> </p>
+                <p>Estado Civil:<br/><input type="text" name="estadoC"></p>
+                
+                <p><input type="submit" value="enviar datos" name="submit"></p> 
+                
+                <?php
+                include("../includes/validar_formulario.php");
+                ?>
 	</form>
 </body>
 </html>
